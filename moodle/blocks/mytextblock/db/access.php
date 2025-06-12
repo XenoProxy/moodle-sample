fa629fd086aa3f46c2d63d6fd44731475a0a2086
xenoproxy <xenoproxy@yandex.by>
2025-06-12 11:15:35 +0200

    update simple custom block

--- moodle/blocks/mytextblock/db/access.php
+++ moodle/blocks/mytextblock/db/access.php
@@ -3,16 +3,22 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-    // Возможность добавления экземпляра этого блока.
-    // Это стандартная возможность, которую ДОЛЖНЫ определять ВСЕ блоки.
-    'block/mytextblock:addinstance' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_SYSTEM, // Может быть добавлен на уровне сайта.
+  'block/mytextblock:myaddinstance' => [
+      'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'manager' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW, // Учителя могут добавлять блоки на страницы курсов.
+            'editingteacher' => CAP_ALLOW,
             'coursecreator' => CAP_ALLOW,
         ),
-        'riskbitmask' => RISK_SPAM | RISK_XSS, // Стандартные риски для пользовательского контента.
-    ),
+    ],
+    'block/mytextblock:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+        ),
+    ]
 );
--- moodle/blocks/mytextblock/lang/en/block_mytextblock.php
+++ moodle/blocks/mytextblock/lang/en/block_mytextblock.php
@@ -1,6 +1,8 @@
 <?php
 
-$string['pluginname'] = 'My Simple Text Block'; // Название блока в списке плагинов
-$string['mytextblock'] = 'My Simple Text Block'; // Заголовок, который будет отображаться в блоке
-$string['hellomessage'] = 'Hello, Moodle World!'; // Пример текста
-$string['simpledescription'] = 'This is a very basic Moodle block that just displays some text.'; // Пример текста
+$string['pluginname'] = 'My Simple Text Block';
+$string['mytextblock'] = 'My Simple Text Block'; 
+$string['hellomessage'] = 'Hello, Moodle World!'; 
+$string['simpledescription'] = 'This is a very basic Moodle block that just displays some text.'; 
+$string['mytextblock:addinstance'] = 'Add a new pluginname block';
+$string['mytextblock:myaddinstance'] = 'Add a new pluginname block to the My Moodle page';
