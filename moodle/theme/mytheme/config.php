0c24d437a0fae34276975892006c61d73d61bc27
xenoproxy <xenoproxy@yandex.by>
2025-06-13 14:29:10 +0200

    update mytheme

--- moodle/theme/mytheme/config.php
+++ moodle/theme/mytheme/config.php
@@ -4,8 +4,12 @@ defined('MOODLE_INTERNAL') || die();
 
 $THEME->name = 'mytheme';
 $THEME->parents = ['boost']; 
-$THEME->version = 2025061303;
-$THEME->sheets = ['mytheme'];
+$THEME->version = 2025061304;
+$THEME->sheets = [];
+
+// $THEME->scss = function($theme) {
+//   theme_mytheme_get_main_scss_content($theme);
+// };
 
 $THEME->plugin_settings = [];
 $THEME->layouts = [];
--- moodle/theme/mytheme/version.php
+++ moodle/theme/mytheme/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_mytheme';     
-$plugin->version = 2025061303;            
+$plugin->version = 2025061304;            
 $plugin->requires = 2022041900;           
 $plugin->maturity = MATURITY_STABLE;       
 $plugin->release = '1.0';                  
